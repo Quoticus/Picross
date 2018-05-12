28ad7a581e09a08c0f8a57478f25470e58b559b7
Quoticus <38863921+Quoticus@users.noreply.github.com>
2018-05-12 18:25:57 -0300

    Started adding the bulk of the code.
    HTML Doc
    - Added the placeholder table with classes attached
    
    JS Doc
    - Started writing the click events and functions for each cell
    - Cells should now Left and Right mouse button clickable
    - Cells should properly swap between each type of cell click
    - In the background a new property of "selected" is being added to each TD and swapped between "true" and "false" depending on if it is clicked.
    
    CSS Doc
    - Added Table, Table Td, .cell, and .hint stylings
    - There is a secondary .hint right now that is overriding control of all hint classes that'll need to be selector'd out to only select the top row of "hints"

--- js/picrossGame.js
+++ js/picrossGame.js
@@ -3,4 +3,36 @@ Author: Quoticus
 Date: May 12th, 2018
 Purpose: JavaScipt file for Picross Website app
 */
-window.onload = function(){$(document).click(function(){});}
+window.onload = function(){
+    var tdArray = $(".cell");
+    tdArray.each(function(){
+      let cell = $(this);
+      cell.click(function(){
+        if(cell.prop("selected") == true){
+          cell.prop("selected", false).css("background-color", "white").text("");
+          //console.log("white");
+        }
+        else{
+          cell.prop("selected", true).css("background-color", "black").text("");
+          //console.log("black");
+        }
+        //console.log(cell);
+      });
+      cell.contextmenu(function(event){
+        event.preventDefault();
+        if(cell.prop("selected") == true){
+          cell.prop("selected", false).css("background-color", "white");
+        }
+        if(cell.text() == "X"){
+          cell.text("");
+        }
+        else{
+          cell.text("X");
+        }
+      });
+    });
+    //console.log(tdArray);
+}
+
+
+//Each cell has a cellIndex, and its parent element, will have a rowIndex.
