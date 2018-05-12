250222d308f29216574b9669f8ae27f527fa321d
Quoticus <38863921+Quoticus@users.noreply.github.com>
2018-05-12 20:16:46 -0300

    Added answer checking
    Current from will check user answers against answer array everytime a square has been changed.
    
    When all squares are as they should be (IE when Right Answers equals Total Answers) the puzzle will evaluate as true and give the user a "You win!' text string.
    
    This'll need to be jazz'd up in a future update.

--- js/picrossGame.js
+++ js/picrossGame.js
@@ -5,18 +5,28 @@ Purpose: JavaScipt file for Picross Website app
 */
 window.onload = function(){
     var tdArray = $(".cell");
+    var answerArray = [];
+    for(var x = 0; x < 6; x++){
+      answerArray[x] = [];
+      for(var y = 0; y < 6; y++){
+        if((Math.random() * 100) >= 60){
+          answerArray[x][y] = true;
+        }else {
+          answerArray[x][y] = false;
+        }
+      }
+    }
+
     tdArray.each(function(){
       let cell = $(this);
       cell.click(function(){
         if(cell.prop("selected") == true){
           cell.prop("selected", false).css("background-color", "white").text("");
-          //console.log("white");
         }
         else{
           cell.prop("selected", true).css("background-color", "black").text("");
-          //console.log("black");
         }
-        //console.log(cell);
+        checkForWin(verifyAnswers(tdArray, answerArray));
       });
       cell.contextmenu(function(event){
         event.preventDefault();
@@ -29,10 +39,49 @@ window.onload = function(){
         else{
           cell.text("X");
         }
+        checkForWin(verifyAnswers(tdArray, answerArray));
       });
     });
-    //console.log(tdArray);
+    console.log(tdArray); //Debug - remove before release
+    console.dir(answerArray); //Debug - remove before release
 }
 
+function verifyAnswers(tdArray, answerArray){
+  let on = 0;
+  let off = 0;
+  let right = 0;
+  let wrong = 0;
+  let total = 0;
+  tdArray.each(function(){
+    let cell = $(this);
+    total++;
+    if(cell.prop("selected") == true){
+      on++;
+      if(answerArray[cell.parent().prop("rowIndex")][cell.prop("cellIndex")]){
+        right++;
+      }else{
+        wrong++;
+      }
+    }else{
+      off++;
+      if(answerArray[cell.parent().prop("rowIndex")][cell.prop("cellIndex")]){
+        wrong++;
+      }else{
+        right++;
+      }
+    }
+  });
+  //console.log("On: "+on+"\nOff: "+off);
+  console.log("Right: "+right+"\nWrong: "+wrong); //Debug - remove before release
+  if(right == total){
+    return true;
+  }else{
+    return false;
+  }
+}
 
-//Each cell has a cellIndex, and its parent element, will have a rowIndex.
+function checkForWin(winCondition){
+  if(winCondition){
+    $("#vic").text("You win!");
+  }
+}
